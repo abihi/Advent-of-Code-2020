4be2a2347dec42166200229cb94bf0b18ae2e366
abihi <ahmed.bihi@hotmail.com>
2020-12-01 17:42:14 +0100

    day 1

---
+++ day1/q1.py
@@ -0,0 +1,19 @@
+example_input = [int(x) for x in "1721 979 366 299 675 1456".split(" ")]
+
+def find_2020(number_list):
+    for x in number_list:
+        for y in number_list:
+            for z in number_list:
+                if x + y + z == 2020:
+                    return x*y*z
+
+def process_input(filename):
+    number_list = list()
+    with open(filename) as input_file:
+        for line in input_file:
+            number_list.append(int(line))
+    return number_list
+
+number_list = process_input('q1_input.txt')
+print("example answer:", find_2020(example_input))
+print("puzzle answer:", find_2020(number_list))
