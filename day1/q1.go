edbaee8d5876b0057b7f63050702048d12869917
abihi <ahmed.bihi@hotmail.com>
2020-12-02 13:10:19 +0100

    day1 - GO

---
+++ day1/q1.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func find2020(numberList []int) int {
+	for _, x := range numberList {
+		for _, y := range numberList {
+			for _, z := range numberList {
+				if x+y+z == 2020 {
+					return x * y * z
+				}
+			}
+		}
+	}
+	return 0
+}
+
+func preprocesInput(filename string) []int {
+	numberList := make([]int, 0)
+	file, _ := os.Open(filename)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		num, _ := strconv.Atoi(scanner.Text())
+		numberList = append(numberList, num)
+	}
+
+	return numberList
+}
+
+func main() {
+	var numberList = preprocesInput("q1_input.txt")
+	var answer = find2020(numberList)
+	fmt.Println(answer)
+}
