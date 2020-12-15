8c4e67e68eccfd1386dc93e4264db1b8f2a5f3aa
abihi <ahmed.bihi@hotmail.com>
2020-12-15 23:48:18 +0100

    day11 p2

--- day11/day11.go
+++ day11/day11.go
@@ -4,49 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
-
-func paddingRow(length int, padSymbol string) []string {
-	row := make([]string, length)
-	for i := range row {
-		row[i] = padSymbol
-	}
-	return row
-}
-
-func padLine(line string, padSymbol string) []string {
-	row := []string{padSymbol}
-	for _, c := range line {
-		if c != '\n' {
-			row = append(row, string(c))
-		}
-	}
-	row = append(row, padSymbol)
-	return row
-}
 
 func generateBoard(filename string) [][]string {
 	board := make([][]string, 0)
 	file, _ := os.Open(filename)
 	scanner := bufio.NewScanner(file)
 
-	first := true
 	for scanner.Scan() {
 		line := scanner.Text()
-		if first {
-			board = append(board, paddingRow(len(line)+2, "."))
-			first = false
-		}
-		board = append(board, padLine(line, "."))
+		board = append(board, strings.Split(strings.TrimSuffix(line, "\n"), ""))
 	}
-	board = append(board, paddingRow(len(board[0]), "."))
 	return board
 }
 
 func occupiedSeats(board [][]string) int {
 	occupied := 0
-	for i := 1; i < len(board)-1; i++ {
-		for j := 1; j < len(board)-1; j++ {
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
 			if board[i][j] == "#" {
 				occupied++
 			}
@@ -55,30 +31,27 @@ func occupiedSeats(board [][]string) int {
 	return occupied
 }
 
-func getNeighbors(board [][]string, i int, j int) string {
-	top := board[i-1][j-1] + board[i-1][j] + board[i-1][j+1]
-	mid := board[i][j-1] + board[i][j+1]
-	bot := board[i+1][j-1] + board[i+1][j] + board[i+1][j+1]
-	return top + mid + bot
-}
-
-func countSymbol(neighbors string, symbol rune) int {
-	count := 0
-	for _, c := range neighbors {
-		if c == symbol {
-			count++
+func countNeighbors(board [][]string, i int, j int) int {
+	neighbors := 0
+	for r := -1; r <= 1; r++ {
+		for c := -1; c <= 1; c++ {
+			if r == 0 && c == 0 {
+				continue
+			}
+			tb := i + r
+			lr := j + c
+			for (0 <= tb && tb < len(board)) && (0 <= lr && lr < len(board[0])) && board[tb][lr] == "." {
+				tb += r
+				lr += c
+			}
+			if (0 <= tb && tb < len(board)) && (0 <= lr && lr < len(board[0])) {
+				if board[tb][lr] == "#" {
+					neighbors++
+				}
+			}
 		}
 	}
-	return count
-}
-
-func symbolNotIn(neighbors string, symbol rune) bool {
-	for _, c := range neighbors {
-		if c == symbol {
-			return false
-		}
-	}
-	return true
+	return neighbors
 }
 
 func applyRules(board [][]string) ([][]string, bool) {
@@ -90,40 +63,35 @@ func applyRules(board [][]string) ([][]string, bool) {
 		copy(nextBoard[i], board[i])
 	}
 
-	for i := 1; i < len(board)-1; i++ {
-		for j := 1; j < len(board)-1; j++ {
-			neighbors := getNeighbors(board, i, j)
-			if board[i][j] == "L" && symbolNotIn(neighbors, '#') {
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			neighbors := countNeighbors(board, i, j)
+			if board[i][j] == "L" && neighbors == 0 {
 				nextBoard[i][j] = "#"
 				changed = true
-			} else if board[i][j] == "#" && countSymbol(neighbors, '#') >= 4 {
+			} else if board[i][j] == "#" && neighbors >= 5 {
 				nextBoard[i][j] = "L"
 				changed = true
 			}
 		}
 	}
 
-	// for i := range nextBoard {
-	// 	fmt.Println("#####")
-	// 	fmt.Println(board[i])
-	// 	fmt.Println(nextBoard[i])
-	// }
-
 	return nextBoard, changed
 }
 
 func main() {
+	changed := false
 	board := generateBoard("day11.in")
-	nextBoard, changed := applyRules(board)
 
 	i := 0
-	for changed {
-		nextBoard, changed = applyRules(nextBoard)
+	for true {
+		board, changed = applyRules(board)
+		if !changed {
+			break
+		}
 		i++
-		fmt.Println(i)
 	}
 
-	fmt.Println("P1:", occupiedSeats(nextBoard))
-
-	// fmt.Println(changed)
+	fmt.Println("P2:", occupiedSeats(board))
+	fmt.Println(i)
 }
