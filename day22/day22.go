7e15a7e9e848793ba11ae0cd9cd4bf6bca9d7328
abihi <ahmed.bihi@hotmail.com>
2021-01-10 01:18:02 +0100

    Day 22 Go

--- day22/day22.go
+++ day22/day22.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"strconv"
 	"strings"
+
+	"github.com/tebeka/deque"
 )
 
 func toInt(s []string) []int {
@@ -26,25 +28,47 @@ func score(player []int) int {
 	return score
 }
 
-func recursiveGame(player1 []int, player2 []int) ([]int, []int) {
-	for true {
-		if len(player1) == 0 || len(player2) == 0 {
-			break
-		}
-		p1 := player1[0]
-		p2 := player2[0]
-		player1 = player1[1:]
-		player2 = player2[1:]
+type Decks struct {
+	D1, D2 *deque.Deque
+}
 
-		if p1 > p2 {
-			player1 = append(player1, p1)
-			player1 = append(player1, p2)
-		} else if p2 > p1 {
-			player2 = append(player2, p2)
-			player2 = append(player2, p1)
+func recursiveGame(D1 *deque.Deque, D2 *deque.Deque) (bool, *deque.Deque) {
+	SEEN := map[Decks]bool{}
+	for D1.Len() > 0 && D2.Len() > 0 {
+		decks := Decks{D1, D2}
+		if _, ok := SEEN[decks]; ok {
+			return true, D1
+		}
+		SEEN[decks] = true
+		c1, _ := D1.PopLeft()
+		c2, _ := D2.PopLeft()
+		var p1_wins bool
+		if D1.Len() >= c1.(int) && D2.Len() >= c2.(int) {
+			NewD1, NewD2 := deque.New(), deque.New()
+			for x := 0; x < c1.(int); x++ {
+				c, _ := D1.Get(x)
+				NewD1.Append(c.(int))
+			}
+			for x := 0; x < c2.(int); x++ {
+				c, _ := D2.Get(x)
+				NewD2.Append(c.(int))
+			}
+			p1_wins, _ = recursiveGame(NewD1, NewD2)
+		}
+
+		if p1_wins {
+			D1.Append(c1)
+			D1.Append(c2)
+		} else {
+			D2.Append(c2)
+			D2.Append(c1)
 		}
 	}
-	return player1, player2
+
+	if D1.Len() > 0 {
+		return true, D1
+	}
+	return false, D2
 }
 
 func game(player1 []int, player2 []int) ([]int, []int) {
@@ -74,10 +98,28 @@ func main() {
 	player1 := toInt(strings.Split(players[0], "\n")[1:])
 	player2 := toInt(strings.Split(players[1], "\n")[1:])
 
+	D1, D2 := deque.New(), deque.New()
+	for i := range player1 {
+		D1.Append(player1[i])
+		D2.Append(player2[i])
+	}
+
 	player1, player2 = game(player1, player2)
 	if len(player1) == 0 {
-		fmt.Println(score(player2))
+		fmt.Println("P1", score(player2))
 	} else {
-		fmt.Println(score(player1))
+		fmt.Println("P1", score(player1))
 	}
+
+	_, deck := recursiveGame(D1, D2)
+	winner_deck := []int{}
+	for deck.Len() > 0 {
+		c, _ := deck.PopLeft()
+		winner_deck = append(winner_deck, c.(int))
+	}
+	var score int = 0
+	for i, c := range winner_deck {
+		score += (len(winner_deck) - i) * c
+	}
+	fmt.Println("p2", score)
 }
