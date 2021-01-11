9225b6d4f71625b7ec2e62760516ad8bdd0fff4a
abihi <ahmed.bihi@hotmail.com>
2021-01-11 14:35:21 +0100

    Day 23

--- day23/day23.go
+++ day23/day23.go
@@ -19,70 +19,82 @@ func in(i int, s []int) bool {
 	return false
 }
 
-func findDestination(curr int, circle []int) int {
-	for i, label := range circle {
-		if curr == label {
-			return i
-		}
+func solve(input string, isP2 bool) string {
+	var n int
+	if isP2 {
+		n = int(1e6)
+	} else {
+		n = len(input)
 	}
-	return -1
+	N := make([]int, n+1)
+	X := []int{}
+	for _, in := range input {
+		num, _ := strconv.Atoi(string(in))
+		X = append(X, num)
+	}
+
+	for i := 0; i < len(X); i++ {
+		N[X[i]] = X[(i+1)%len(X)]
+	}
+
+	if isP2 {
+		N[X[len(X)-1]] = len(X) + 1
+		for i := 0; i < len(X)+1; i = i + (n + 1) {
+			N[i] = i + 1
+		}
+		N[len(N)-1] = X[0]
+	}
+
+	t := 0
+	current := X[0]
+	nMoves := 100
+	var dest int
+	for moves := nMoves; 0 < moves; moves-- {
+		t++
+		pickup := N[current]
+		N[current] = N[N[N[pickup]]]
+
+		if current == 1 {
+			dest = n
+		} else {
+			dest = current - 1
+		}
+
+		slice := []int{pickup, N[pickup], N[N[pickup]]}
+		for in(dest, slice) {
+			fmt.Println(dest, slice)
+			if dest == 1 {
+				dest = n
+			} else {
+				dest--
+			}
+		}
+		N[N[N[pickup]]] = N[dest]
+		N[dest] = pickup
+		current = N[current]
+	}
+
+	if isP2 {
+		fmt.Println("P2:", N[1]*N[N[1]])
+	}
+
+	tmp := []int{}
+	x := N[1]
+	for x != 1 {
+		tmp = append(tmp, x)
+		x = N[x]
+	}
+
+	ans := ""
+	for _, i := range tmp {
+		ans += fmt.Sprint(i)
+	}
+	return ans
 }
 
 func main() {
 	input, _ := ioutil.ReadFile("day23_ex.in")
-	circle := []int{}
-	cup := Cup{}
-	min := 10000
-	max := 0
-	for i, in := range input {
-		num, _ := strconv.Atoi(string(in))
-		if num < min {
-			min = num
-		}
-		if num > max {
-			max = num
-		}
-		if i == 0 {
-			cup = Cup{num, i}
-			circle = append(circle, num)
-		} else {
-			circle = append(circle, num)
-		}
-	}
 
-	moves := 2
-	for moves > 0 {
-		current := cup.label
-		currentPos := cup.pos
-		puPos := []int{(cup.pos) % len(circle), (cup.pos + 1) % len(circle), (cup.pos + 2) % len(circle)}
-		pickedUp := []int{circle[puPos[0]], circle[puPos[1]], circle[puPos[2]]}
-		destinationPos := -1
-
-		for true {
-			current--
-			if current < min {
-				current = max
-			}
-			destinationPos = findDestination(current, circle)
-			if destinationPos != -1 && !in(circle[destinationPos], pickedUp) {
-				cup.label = circle[destinationPos]
-				cup.pos = currentPos
-				break
-			}
-		}
-
-		for _, cup := range circle[destinationPos:] {
-			pickedUp = append(pickedUp, cup)
-		}
-
-		circle = circle[:destinationPos]
-
-		for _, cup := range pickedUp {
-			circle = append(circle, cup)
-		}
-
-		moves--
-	}
-
-	fmt.Println(circle)
+	fmt.Println(solve(string(input), false))
+	//fmt.Println(solve(string(input), true))
 }
